e9db69675b37cd1abf2c4b7e1edeca1897f7ed30
Sandeep Kumar <sands_hell@Sandeeps-MacBook-Pro.local>
2018-10-01 20:11:21 +0530

    Initial Commit

---
+++ 3DTouchTemplateTests/_DTouchTemplateTests.swift
@@ -0,0 +1,34 @@
+//
+//  _DTouchTemplateTests.swift
+//  3DTouchTemplateTests
+//
+//  Created by Sandeep Kumar on 01/10/18.
+//  Copyright © 2018 Sandeep Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import _DTouchTemplate
+
+class _DTouchTemplateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
